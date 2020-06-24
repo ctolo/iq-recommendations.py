40ff1f01a7ce3dbf9589b18be90020aa355e59d3
cmorenoserrano <cmorenoserrano@sonatype.com>
2020-06-24 18:05:20 +0100

    pdf is generated

--- iq-recommendations.py
+++ iq-recommendations.py
@@ -5,6 +5,7 @@ import requests
 import time
 import datetime
 import json, argparse
+from fpdf import FPDF
 
 ## ----------------------------------------------------------------------------
 t0= time.clock()
@@ -25,6 +26,151 @@ def getArguments():
 	iq_session.auth = requests.auth.HTTPBasicAuth(creds[0], creds[1])
 	return args
 #-----------------------------------------------------------------------------
+
+#---------------------------------
+
+class PDF(FPDF):
+    def header(self):
+        # Logo
+        self.image('sonatype_logo.png', 10, 8, 33)
+        # Times bold 15
+        self.set_font('Times', 'B', 15)
+        # Move to the right
+        self.cell(80)
+        # Title
+        self.cell(100, 10, 'Remediation report', 1, 0, 'C')
+        # Line break
+        self.ln(20)
+
+        # Page footer
+    def footer(self):
+        # Position at 1.5 cm from bottom
+        self.set_y(-15)
+        # Arial italic 8
+        self.set_font('Times', 'I', 8)
+        # Page number
+        self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
+
+        #Chapter title
+    def chapter_title(self, title):
+        # Arial 12
+        self.set_font('Times', 'B', 12)
+        # Background color
+        self.set_fill_color(200, 220, 255)
+        # Title
+        self.cell(0, 6, '%s' % (title), 0, 1, 'L', 1)
+        # Line break
+        self.ln(0)
+
+        #Chapter body
+    def chapter_body(self, content_dict):
+        # Times 12
+        self.set_font('Times', '', 12)
+        # Output justified text
+        #self.multi_cell(0, 5, content)
+        for field in content_dict:
+            self.cell(0, 5, field+": "+content_dict[field], 1, 1)
+        # Line break
+        self.ln()
+
+        #Print chapter
+    def print_chapter(self, title, content):
+        self.add_page('L')
+        self.chapter_title(title)
+        self.chapter_body(content)
+
+    def print_list(self,data):
+        self.cell()
+
+    def fancy_table(this,header,data):
+        #Colors, line width and bold font
+        this.set_fill_color(255,0,0)
+        this.set_text_color(255)
+        this.set_draw_color(128,0,0)
+        this.set_line_width(.3)
+        this.set_font('Times','B')
+        #Header
+        w=[]
+        column_no = len(header)
+        page_width = 277 #magic number for A4 in mm
+        column_width = page_width/column_no
+        for i in range(0,column_no):
+            w.append(column_width)
+        for i in range(0,column_no):
+                this.cell(w[i],7,header[i],1,0,'C',1)
+        this.ln()
+        #Color and font restoration
+        this.set_fill_color(224,235,255)
+        this.set_text_color(0)
+        this.set_font('Times')
+        #Data
+        fill=0
+        #print("This data: ")
+        #print(len(data))
+        #print(len(w))
+        #print(column_no)
+        for row in data:
+            for i in range(0,column_no):
+                this.cell(w[i],6,row[i],'LR',0,'C',fill)
+                #print(row[i])
+            this.ln()
+            fill=not fill
+        this.cell(sum(w),0,'','T')
+
+    def dynamic_table(this,header,data):
+        #Colors, line width and bold font
+        this.set_fill_color(255,0,0)
+        this.set_text_color(255)
+        this.set_draw_color(128,0,0)
+        this.set_line_width(.3)
+        this.set_font('Times','B')
+        #Header
+        w=[]
+        column_no = len(header)
+        page_width = 277 #magic number for A4 in mm
+        column_width = page_width/column_no
+        for i in range(0,column_no):
+            w.append(column_width)
+        for i in range(0,column_no):
+                this.cell(w[i],7,header[i],1,0,'C',1)
+        this.ln()
+        #Color and font restoration
+        this.set_fill_color(224,235,255)
+        this.set_text_color(0)
+        this.set_font('Times')
+        #Data
+        fill=0
+        #print("This data: ")
+        #print(len(data))
+        #print(len(w))
+        #print(column_no)
+        for row in data:
+            for i in range(0,column_no):
+                this.multi_cell(w[i],6,row[i],1,'L',fill)
+                fill=not fill
+                this.multi_cell(w[i],6,row[i+1],1,'L',fill)
+                fill=not fill
+                #this.multi_cell(w[i],6,row[i],0,'C',fill)
+                #this.cell(w[i],6,row[i],'LR',0,'C',fill)
+                #print(row[i])
+            this.ln()
+        this.cell(sum(w),0,'','T')
+
+#---------------------------------
+
+def output_pdf(pages, filename):
+	pdf = FPDF()
+	pdf.set_font('Times','B',12)
+	for image in pages:
+		pdf.add_page('L')
+		pdf.set_xy(0,0)
+		pdf.image(image, x = None, y = None, w = 0, h = 0, type = '', link = '')
+	pdf.output(filename, 'F')
+
+
+#---------------------------------
+
+
 #-----------------------------------------------------------------------------
 def main():
 	args = getArguments()
@@ -57,16 +203,43 @@ def main():
 
 	dumps(final)
 	print("Final results saved to -> results.json")
-#-----------------------------------------------------------------------------
+	pdf = PDF()
+	pdf.alias_nb_pages()
+	components = final
+	#print(components)
+	header = ['Current component (white), Recommended version (blue)']
+	#header = ['Current component','Next with no violations','Next non-failing']
+	data = []
+	for component in components:
+                if component["packageUrl"]["packageUrl"] is not None:
+                        current = component["packageUrl"]["packageUrl"]
+                        no_violations = component["recommendation"]["remediation"]["versionChanges"][0]["data"]["component"]["packageUrl"]
+                        #non_failing = component["recommendation"]["remediation"]["versionChanges"][1]["data"]["component"]["packageUrl"]
+                        aux = [current,no_violations]
+                        #aux = [current,no_violations,non_failing]
+                        data.append(aux)
+                        #print(data)
+	pdf.print_chapter('Remediation recommendations for '+str(publicId),"")
+	pdf.set_font('Times','B',18)
+	pdf.set_text_color(0,0,0)
+	instructions = "Note: if the recommended version is the same as the current one, then there is no clean version without violations"
+	pdf.multi_cell(0,7,instructions,0)
+	pdf.ln(15)
+	pdf.set_font('Times','',12)
+	pdf.dynamic_table(header,data)
+	pdf.output('./remediation_report.pdf', 'F')
+	print("PDF generated -> remediation_report.pdf")
+
+	
 #-----------------------------------------------------------------------------
 def pp(c):
 	print( json.dumps(c, indent=4) )
 
 def dumps(page, pretty = True, file_name = "results.json"):
 	try:
-		if pretty: page = json.dumps(page, indent=4)
-		with open(file_name,"w+") as file:
-			file.write(page)
+                if pretty: page = json.dumps(page, indent=4)
+                with open(file_name,"w+") as file:
+                        file.write(page)
 	finally:
 		return page
 
